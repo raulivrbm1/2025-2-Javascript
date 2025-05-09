646a7bc6f415807cc85e18bec87a3a1e0152a354
rodrigoBermejo <rodrigo@inadaptados.mx>
2025-05-08 22:08:47 -0600

    Día 4

--- semana1/clase4_tarea.js
+++ semana1/clase4_tarea.js
@@ -7,8 +7,24 @@
 
 const { ask } = require('../helpers/input');
 
-async function main() {
+function diffDays(actualDate, birthDate) {
+  const diff = actualDate - birthDate;
+  const edadEnDias = Math.floor(diff / (1000 * 60 * 60 * 24));
 
+  return edadEnDias;
+}
+
+async function main() {
+  const actualDate = new Date();
+
+  const birthDateDay = await ask("¿Cuál es el día de tu fecha de nacimiento?");
+  const birthDateMonth = await ask("¿Cuál es el mes de tu fecha de nacimiento?");
+  const birthDateYear = await ask("¿Cuál es el año de tu fecha de nacimiento?");
+  //const birthDate = await ask("¿Cuál es tu fecha de nacimiento? (formato YYYY-MM-DD)");
+  const birthDate = new Date(`${birthDateYear}-${birthDateMonth}-${birthDateDay}`);
+
+  console.log("Haz vivido aproximadamente: ");
+  console.log(`${diffDays(actualDate, birthDate)} días`);
 
 }
 
--- semana1/ejercicio4.js
+++ semana1/ejercicio4.js
@@ -8,23 +8,23 @@
 const { ask } = require('../helpers/input');
 
 // TODO: Función que devuelve el mayor de dos números
-
-function obtenerMayor(num1, num2) {
-  if (num1 > num2) {
-    return "Numero 1 es mayor" + num1;
+function obtenerMayor(a, b) {
+  if (a > b) {
+    return a;
   } else {
-    return "Numero 2 es mayor" + num2;
+    return b;
   }
 }
 
 async function main() {
-  const num1 = Number(await ask("Ingresa el primer número"));
-  const num2 = Number(await ask("Ingresa el segundo número"));
+  const num1 = Number(await ask("Introduce el primer número a evaluar: "));
+  const num2 = Number(await ask("Introduce el segundo número a evaluar: "));
+
   if (num1 === num2) {
-    console.log("Lo siento, los números que introduciste son iguales, no es posible evaluarlos");
+    console.log("Los dos numeros que ingresaste son iguales");
   } else {
     const resultado = obtenerMayor(num1, num2);
-    console.log(resultado);
+    console.log("El numero mayor es: ", resultado);
   }
 
 }
