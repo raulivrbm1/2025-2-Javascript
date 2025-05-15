1b44ce06be129a95753f31680898ec40f6724c59
raulivrbm1 <raulivrbm@gmail.com>
2025-05-14 21:40:40 -0600

    Semana2

--- semana2/clase2_tarea.js
+++ semana2/clase2_tarea.js
@@ -11,3 +11,56 @@
 // 2. Eliminar fruta
 // 3. Ver lista de frutas
 // 4. Salir
+const { ask } = require('../helpers/input');
+
+async function main(){
+    
+    let opcion = "";
+    let frutas = [];
+
+    while (opcion !== 4){
+        opcion = await ask ("¿Qué deseas hacer? \n1. Agregar una fruta \n2. Eliminar una fruta \n3. Ver lista de frutas \n4. Salir");
+        
+        if (opcion === "1"){
+            let frutaNueva = await ask('¿Qué fruta deseas agregar?: ');
+            frutas.push(frutaNueva);
+            console.log(`Fruta agregada: ${frutaNueva}`);
+            console.log('Frutas disponibles: ', frutas);
+            } else if (opcion === '2'){
+                if (frutas.length === 0) {
+                    console.log('No hay frutas disponibles');
+                    continue;
+                  } else {
+                    let frutaEliminar = await ask('¿Qué fruta deseas eliminar?: ');
+                    let index = frutas.indexOf(frutaEliminar);
+            
+                    if (index !== -1) {
+                      frutas.splice(index, 1);
+                      console.log(`${frutaEliminar} ha sido eliminada `)
+                    } else {
+                      console.log('Fruta no encontrada ');
+                    }
+                    console.log('Frutas disponibles: ', frutas);
+                  }
+            } else if (opcion === '3') {
+                if (frutas.length >= 0) {
+                    console.log ('La lista de frutas es: ', frutas );
+                
+                } else {
+                    console.log('No hay frutas');
+                   
+                }
+            } else if (opcion === '4') {
+            
+                console.log('Gracias por usar el programa');
+              } else {
+                console.log('Opción no válida');
+              }
+            
+
+
+        }
+    }
+    main();
+
+
