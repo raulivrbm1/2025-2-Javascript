82f893a92705b365498ac591f2f188f0bd222479
rodrigoBermejo <rodrigo@inadaptados.mx>
2025-05-14 21:38:02 -0600

    update ejercicio3 semana 2

---
+++ semana2/clase3_tarea.js
@@ -0,0 +1,11 @@
+// 🏠 Práctica para casa:
+// Contar cuántos de una lista de edades pueden votar
+//
+// Instrucciones:
+// Crea un arreglo de edades (pedirlo al usuario).
+//
+// Recorre el arreglo.
+//
+// Cuenta cuántos tienen 18 años o más.
+//
+// Muestra el total.
