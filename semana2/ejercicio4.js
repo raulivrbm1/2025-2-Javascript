74580f7afa68a8ca092f855ead725933289bc7cf
rodrigoBermejo <rodrigo@inadaptados.mx>
2025-05-15 21:38:40 -0600

    dia 4 semana 2 ejercicios

--- semana2/ejercicio4.js
+++ semana2/ejercicio4.js
@@ -10,6 +10,35 @@ function obtenerPromedio(numeros) {
   return promedio;
 }
 
+function obtenerMayor(numeros) {
+  let mayor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > mayor) {
+      mayor = numeros[i];
+    }
+  }
+  return mayor;
+}
+
+function obtenerMenor(numeros) {
+  let menor = numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] < menor) {
+      menor = numeros[i];
+    }
+  }
+  return menor;
+}
+
+function resumenEstadistico(numeros) {
+  const promedio = obtenerPromedio(numeros);
+  const mayor = obtenerMayor(numeros);
+  const menor = obtenerMenor(numeros);
+
+  return { menor, mayor, promedio }
+  return { 'menor': menor, 'mayor': mayor, 'promedio': promedio }
+}
+
 async function main() {
   const edades = [20, 18, 25, 30, 22];
   const lista = [5, 20, 8, 99, 3];
@@ -17,6 +46,12 @@ async function main() {
   const nombres = ["Ana", "Esteban", "Luis", "Oscar", "María"];
 
   console.log(`Promedio = ${obtenerPromedio(edades)}`);
+  console.log(`El número mayor de ${lista} = ${obtenerMayor(lista)}`);
+  const estadistica = resumenEstadistico(datos);
+  console.log(`El resumen estadístico de: ${datos}`);
+  console.log(`El número menor es: ${estadistica.menor}`);
+  console.log(`El número mayor es: ${estadistica.mayor}`);
+  console.log(`El promedio es: ${estadistica.promedio}`);
 }
 
 main();
