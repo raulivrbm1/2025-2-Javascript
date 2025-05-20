aa6c6c428a4870bbf07cbe6f70c964f348a869fb
rodrigoBermejo <rodrigo@inadaptados.mx>
2025-05-19 20:18:32 -0600

    semana 3 día 1

---
+++ semana3/clase1_tarea.js
@@ -0,0 +1,17 @@
+// 📝 Instrucciones:
+// Crea un arreglo llamado alumnos.
+//
+// Usa un bucle while para permitir al usuario agregar objetos alumno al arreglo.
+// Cada alumno debe tener:
+//
+// nombre
+// edad
+// calificacion
+//
+// Pregunta al usuario si desea seguir agregando más alumnos.
+// Después de salir del ciclo, muestra:
+// Cuántos alumnos tienen calificación aprobatoria (≥ 70).
+// Cuántos pueden votar (edad ≥ 18).
+// El promedio general de calificaciones.
+// La calificación mayor.
+// La calificación menor.
