5fafec4a9924b71bf67cae44c29e34baf90aee92
rodrigoBermejo <rodrigo@inadaptados.mx>
2025-05-22 20:25:13 -0600

    Create test.js

---
+++ concurso/test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { ask } = require('../helpers/input');
+
+async function main() {
+  //const nombre = await ask("¿Cuál es tu nombre?");
+  const nombre = fs.readFileSync(0, 'utf8').trim();
+  console.log("Hola " + nombre);
+
+}
+
+main();
